6bc2648efdc7c98610c9ab572ed1e69c34cdf0bb
hanbrooke22 <146210480+hanbrooke22@users.noreply.github.com>
2023-10-09 14:26:50 +0000

    Worksheet 1 question 4

--- worksheet1/question4.c
+++ worksheet1/question4.c
@@ -3,9 +3,15 @@
 int main(){
 
     float radius;
+    float area; 
 
-    printf("Please enter radius of the circle: ")
-    scanf("%f", radius)
+    printf("Please enter radius of the circle: ");
+    scanf("%f", &radius);
 
-    
+    area = 3.1415 * (radius * radius);
+
+    printf("The area of the circle with the radius %g is %f\n", radius, area);
+
+    return 0;
+
 }
