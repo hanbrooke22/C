241659192d2f63f6ac93bca5c8d9ee517ad36b36
hanbrooke22 <146210480+hanbrooke22@users.noreply.github.com>
2023-10-16 10:04:08 +0000

    Hi

--- week1/basics/age.c
+++ week1/basics/age.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main(){
-       int age; 
+    int age; 
 
     printf("Enter your age: ");
     scanf("%d", &age);
     printf("Your age is %d\n", age);
     
-    return 0; 
+
+    return 0;
 }
