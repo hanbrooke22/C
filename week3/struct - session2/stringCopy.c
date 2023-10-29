f0e9f96f5aa4d9b89fc31a4cd9182645d87deb5b
hanbrooke22 <146210480+hanbrooke22@users.noreply.github.com>
2023-10-29 14:11:08 +0000

    HI

---
+++ week3/struct - session2/stringCopy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct student {
+    char name [20];
+    char student_id [11]; // Assuming 10 digits.
+    unsigned mark;
+};
+
+int main () {
+    struct student new_student;
+
+    strcpy(new_student.name, "Name Surname"); //add to the record
+    strcpy(new_student.student_id, "28932123"); //add to the record 
+    printf("Please enter the mark: "); 
+    scanf("%", new_student.mark); 
+
+    printf("Student name: %s\n", new_student.name);
+    printf("Student ID:   %s\n", new_student.student_id);
+    printf("Final mark:   %u\n", new_student.mark);
+    return 0;
+}
